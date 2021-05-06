26802f59e826b8a96b9c57df62801c400b366934
Noah Huppert <contact@noahh.io>
2021-05-06 19:21:26 -0400

    cleaned up random-report benchmark and added new args

--- src/bench-random-report.rs
+++ src/bench-random-report.rs
@@ -1,9 +1,8 @@
 mod alloc;
-use alloc::{AlligatorAlloc,SizeClass,MIN_SIZE_CLASS,MAX_SIZE_CLASS,FreshReusedStats,AllocMetrics};
+use alloc::{AlligatorAlloc,SizeClass,MIN_SIZE_CLASS,MAX_SIZE_CLASS};
 use alloc::heap::HeapType;
 use core::alloc::Layout;
 use std::alloc::GlobalAlloc;
-use cfg_if::cfg_if;
 use rand::prelude::*;
 use std::env;
 use std::process::exit;
@@ -18,185 +17,287 @@ use std::convert::TryFrom;
 // #[global_allocator]
 static ALLOC: AlligatorAlloc<HeapType> = AlligatorAlloc::INIT;
 
-fn print_usage() {
-    println!("bench-alloc-report.rs - Perform benchmark allocations and output metrics
+/// Implements a pattern of randomly allocation and freeing.
+struct RandomReport {
+    /// Thread random number generator.
+    rng: ThreadRng,
 
-USAGE
+    /// Pointers which should be freed later.
+    free_later: Vec<*mut u8>,
+    
+    /// The number of times the benchmark has performed the main loop.
+    iteration: u64,
 
-    bench-alloc-report.rs [-h,--csv-header|--only-csv-header] <iterations> [<report interval>]
+    /// The total number of bytes which have been allocated.
+    total_alloc_bytes: u64,
 
-OPTIONS
-
-    -h              Display help text
-    --csv-header    Print CSV header row
-    --only-csv-header    Print CSV header row and exit
-
-ARGUMENTS
-
-    <iterations>           The number of random iterations to perform.
-    [<report interval>]    The number of iterations to wait before outputting a report line. Optional, defaults to 100.
-
-BEHAVIOR
-
-    Outputs the results as a CSV table row.
-");
+    /// Range of size classes which are allowed to be allocated.
+    alloc_range: InclusiveRange<u8>,
 }
 
-fn print_metrics(iteration: u64, metrics: AllocMetrics, ratio: FreshReusedStats, total_alloc_bytes: u32) {
-    // Aggregate some fields
-    let mut total_allocs = 0;
-    let mut total_deallocs = 0;
-    
-    let mut fresh_allocs = 0;
-    let mut reused_allocs = 0;
-    
-    for i in MIN_SIZE_CLASS..=MAX_SIZE_CLASS {
-        let size_class = SizeClass::new(i);
-        
-        total_allocs += metrics.total_allocs[size_class.exp_as_idx()];
-        total_deallocs += metrics.total_deallocs[size_class.exp_as_idx()];
-
-        fresh_allocs += ratio.total_alloc_fresh[size_class.exp_as_idx()];
-        reused_allocs += ratio.total_alloc_reused[size_class.exp_as_idx()];
-    }
-
-    // Print results in a CSV table
-    println!("{iteration},{total_alloc_bytes},{total_minipages},{heap_bytes_write},{heap_bytes_read},{total_allocs},{total_deallocs},{fresh_allocs},{reused_allocs}",
-             iteration=iteration,
-             total_alloc_bytes=total_alloc_bytes,
-             total_minipages=metrics.total_minipages,
-             heap_bytes_write=metrics.heap_bytes_write,
-             heap_bytes_read=metrics.heap_bytes_read,
-             total_allocs=total_allocs,
-             total_deallocs=total_deallocs,
-             fresh_allocs=fresh_allocs,
-             reused_allocs=reused_allocs
-    );
-}
-
-fn args_pop_u64(args: &mut Vec<String>, name: String) -> Result<u64, String> {
-    match args.pop() {
-        Some(a) => match a.parse::<u64>() {
-            Ok(a) => Ok(a),
-            Err(e) => Err(format!("error: {} argument \"{}\" could not be converted into u64: {}", name, a, e)),
-        },
-        None => Err(format!("error: {} argument required", name)),
-    }
-}
-
-/// Allocate and free a lot of times.
-#[cfg(feature = "metrics")]
-fn main() {
-    let mut rng = thread_rng();
-    let mut free_later: Vec<*mut u8> = vec!(); // Free these addresses later
-
-    // Used when a layout needs to be passed but it doesn't matter what its value is
-    let dummy_layout = match Layout::from_size_align(8, 1) {
-        Ok(l) => l,
-        Err(e) => panic!("error making dummy Layout: {}", e),
-    };
-    
-    // Parse command line arguments
-    let mut args: Vec<String> = env::args().collect();
-    args.reverse();
-    let prog_name = args.pop().unwrap();
-    args.reverse();
-
-    if args.len() == 0 {
-        eprintln!("error: incorrect arguments");
-        print_usage();
-        exit(1);
-    }
-
-    if args[0] == "-h" || args[0] == "--help" {
-        print_usage();
-        exit(0);
-    } else if args[0] == "--csv-header" || args[0] == "--only-csv-header" {
-        println!("iteration,total_alloc_bytes,total_minipages,heap_bytes_write,heap_bytes_read,total_allocs,total_deallocs,fresh_allocs,reused_allocs");
-
-        if args[0] == "--only-csv-header" {
-            return exit(0);
-        }
-
-        args.reverse();
-        args.pop();
-    }
-
-    let arg_report_interval = match args.len() > 0 {
-        true => args_pop_u64(&mut args, "<report interval>".to_string()).unwrap(),
-        false => 100,
-    };
-    let arg_iterations = args_pop_u64(&mut args, "<iterations>".to_string()).unwrap();
-
-    let mut total_alloc_bytes: u32 = 0;
-
-    for iteration in 0..=arg_iterations {
-        // Allocate
-        let (metrics, ratio) = unsafe {
-            // Choose random size to allocate.
-            let alloc_bytes: u32 = rng.gen_range(2_u32.pow(u32::from(MIN_SIZE_CLASS))..=2_u32.pow(u32::from(MAX_SIZE_CLASS)));
-            total_alloc_bytes += alloc_bytes;
-
-            // Create layout which requests the maximum number of bytes possible for this size class
-            let layout = match Layout::from_size_align(usize::try_from(alloc_bytes).unwrap(), 1) {
-                Ok(l) => l,
-                Err(e) => panic!("error making Layout::from_size_align({}, 1): {}", alloc_bytes, e),
-            };
-
-            // Call allocate
-            let ptr = ALLOC.alloc(layout);
-
-            // Either free immediately or free now
-            let should_free_now: u8 = rng.gen_range(0..10);
-            if should_free_now <= 4 {
-                // Don't immediately free ~40% of allocations.
-                free_later.push(ptr);
-            } else {
-                ALLOC.dealloc(ptr, layout);
-            }
-
-            // Determine if we should free one of the addresses which we left around
-            let should_free_other_old: u8 = rng.gen_range(0..10);
-            if free_later.len() > 0 && should_free_other_old <= 4 {
-                // Free stuff from free_later about 40% of the time
-                let free_idx: usize = rng.gen_range(0..free_later.len());
-                let free_ptr = free_later[free_idx];
-                ALLOC.dealloc(free_ptr, dummy_layout); // Using the wrong layout shouldn't matter
-                free_later.remove(free_idx);
-            }
-
-            // Return metrics
-            let metrics = match ALLOC.metrics() {
-                Some(m) => m,
-                None => panic!("no metrics found after allocations and deallocations were performed"),
-            };
-
-            let ratio = ALLOC.fresh_reused_stats();
-
-            (metrics, ratio)
-        };
-
-        if iteration % arg_report_interval == 0 {
-            print_metrics(iteration, metrics, ratio, total_alloc_bytes);
-        }
-    }
-
-    let (metrics, ratio) = unsafe {
-        // Free the memory we intentionally left laying around.
-        for ptr in free_later.iter() {
-            ALLOC.dealloc(*ptr, dummy_layout);
-        }
-
-
+impl RandomReport {
+    /// Prints a CSV data row based on the current allocator metrics.
+    unsafe fn print_metrics(&mut self) {
+        // Return metrics
         let metrics = match ALLOC.metrics() {
             Some(m) => m,
             None => panic!("no metrics found after allocations and deallocations were performed"),
         };
 
         let ratio = ALLOC.fresh_reused_stats();
+        
+        // Aggregate per size class metrics into totals
+        let mut total_allocs = 0;
+        let mut total_deallocs = 0;
+        
+        let mut fresh_allocs = 0;
+        let mut reused_allocs = 0;
+        
+        for i in MIN_SIZE_CLASS..=MAX_SIZE_CLASS {
+            let size_class = SizeClass::new(i);
+            
+            total_allocs += metrics.total_allocs[size_class.exp_as_idx()];
+            total_deallocs += metrics.total_deallocs[size_class.exp_as_idx()];
 
-        (metrics, ratio)
+            fresh_allocs += ratio.total_alloc_fresh[size_class.exp_as_idx()];
+            reused_allocs += ratio.total_alloc_reused[size_class.exp_as_idx()];
+        }
+
+        // Print results in a CSV table
+        println!("{iteration},{total_alloc_bytes},{total_minipages},{heap_bytes_write},{heap_bytes_read},{total_allocs},{total_deallocs},{fresh_allocs},{reused_allocs}",
+                 iteration=self.iteration,
+                 total_alloc_bytes=self.total_alloc_bytes,
+                 total_minipages=metrics.total_minipages,
+                 heap_bytes_write=metrics.heap_bytes_write,
+                 heap_bytes_read=metrics.heap_bytes_read,
+                 total_allocs=total_allocs,
+                 total_deallocs=total_deallocs,
+                 fresh_allocs=fresh_allocs,
+                 reused_allocs=reused_allocs
+        );
+    }
+
+    /// Allocate a random size.
+    unsafe fn iterate(&mut self) {
+        // Choose random size to allocate.
+        let alloc_bytes: u64 = self.rng.gen_range(2_u64.pow(u32::from(self.alloc_range.min))..=2_u64.pow(u32::from(self.alloc_range.max)));
+        self.total_alloc_bytes += alloc_bytes;
+
+        // Create layout which requests the maximum number of bytes possible for this size class
+        let layout = match Layout::from_size_align(usize::try_from(alloc_bytes).unwrap(), 1) {
+            Ok(l) => l,
+            Err(e) => panic!("error making Layout::from_size_align({}, 1): {}", alloc_bytes, e),
+        };
+
+        // Call allocate
+        let ptr = ALLOC.alloc(layout);
+
+        if ptr.is_null() {
+            panic!("alloc({}) failed: {:?}", alloc_bytes, ALLOC.alloc_failure_cause());
+        }
+
+        // Either free immediately or free at a random later iteration
+        let should_free_now: u8 = self.rng.gen_range(0..10);
+        if should_free_now <= 4 {
+            // Don't immediately free ~40% of allocations.
+            self.free_later.push(ptr);
+        } else {
+            ALLOC.dealloc(ptr, layout);
+        }
+
+        // Determine if we should free one of the addresses which we left around to free at another random time
+        let should_free_other_old: u8 = self.rng.gen_range(0..10);
+        if self.free_later.len() > 0 && should_free_other_old <= 1 {
+            // Free stuff from free_later about 40% of the time
+            let free_idx: usize = self.rng.gen_range(0..self.free_later.len());
+            let free_ptr = self.free_later[free_idx];
+            ALLOC.dealloc(free_ptr, layout); // Using the wrong layout shouldn't matter
+            self.free_later.remove(free_idx);
+        }
+
+        self.iteration += 1;
+    }
+
+    /// Cleanup any remaining allocations which were left. Then print a final line of metrics so we can confirm everything is clean.
+    unsafe fn cleanup(&mut self) {
+        // Used when a layout needs to be passed but it doesn't matter what its value is
+        let dummy_layout = match Layout::from_size_align(8, 1) {
+            Ok(l) => l,
+            Err(e) => panic!("error making dummy Layout: {}", e),
+        };
+        
+        // Free the memory we intentionally left laying around.
+        for ptr in self.free_later.iter() {
+            ALLOC.dealloc(*ptr, dummy_layout);
+        }
+
+        self.print_metrics();
+    }
+}
+
+/// Behavior of printing the CSV header
+enum PrintCSVHeader {
+    /// Print and continue running the benchmark.
+    Continue,
+
+    /// Print then exit.
+    Exit,
+}
+
+/// An inclusive range.
+struct InclusiveRange<T> {
+    /// Minimum.
+    min: T,
+
+    /// Maximum.
+    max: T,
+}
+
+/// Program run arguments.
+struct Args {
+    /// If true will print help text and exit.
+    print_usage: Option<bool>,
+    
+    /// The number of iterations to perform.
+    max_iterations: Option<u64>,
+
+    /// The interval of iterations to print reports.
+    report_interval: Option<u64>,
+
+    /// If the CSV header should be printed.
+    print_csv_header: Option<PrintCSVHeader>,
+
+    /// Defines the range of size classes to allocate.
+    alloc_range: Option<InclusiveRange<u8>>,
+}
+
+impl Args {
+    /// Parse arguments from command line input. Destroys args argument.
+    fn new(args: &mut Vec<String>) -> Args {
+        let mut parsed = Args{
+            print_usage: None,
+            max_iterations: None,
+            report_interval: None,
+            print_csv_header: None,
+            alloc_range: None,
+        };
+        
+        while !args.is_empty() {
+            let arg = args.pop().unwrap();
+
+            if arg == "-h" || arg == "--help" {
+                parsed.print_usage = Some(true);
+            } else if arg == "-i" || arg == "--max-iterations" {
+                parsed.max_iterations = Some(args.pop().unwrap().parse().unwrap());
+            } else if arg == "-r" || arg == "--report-interval" {
+                parsed.report_interval = Some(args.pop().unwrap().parse().unwrap());
+            } else if arg == "-c" || arg == "--csv-header" {
+                parsed.print_csv_header = Some(PrintCSVHeader::Continue);
+            } else if arg == "-C" || arg == "--only-csv-header" {
+                parsed.print_csv_header = Some(PrintCSVHeader::Exit);
+            } else if arg == "-a" || arg == "--alloc" {
+                parsed.alloc_range = Some(InclusiveRange::<u8>{
+                    min: args.pop().unwrap().parse().unwrap(),
+                    max: args.pop().unwrap().parse().unwrap(),
+                });
+            } else {
+                panic!("unknown argument: {}", arg);
+            }
+        }
+
+        // Set defaults
+        if parsed.max_iterations.is_none() {
+            parsed.max_iterations = Some(1000);
+        }
+
+        if parsed.report_interval.is_none() {
+            parsed.report_interval = Some(100);
+        }
+
+        if parsed.alloc_range.is_none() {
+            parsed.alloc_range = Some(InclusiveRange::<u8>{
+                min: MIN_SIZE_CLASS,
+                max: MAX_SIZE_CLASS,
+            });
+        }
+
+        return parsed;
+    }
+
+    /// Print usage help text.
+    fn print_usage() {
+        println!("bench-random-report.rs - Perform random allocations and print metrics as CSV rows
+
+USAGE
+
+    bench-alloc-report.rs [-h] [-i,--max-iterations <num>] [-r,--report-interval <num>] [-c,--csv-header] [-C,--only-csv-header] [-a,--alloc <min> <max>]
+
+OPTIONS
+
+    -h                            Display help text
+    -i,--max-iteration <num>      Number of iterations to run (default 1000)
+    -r,--report-interval <num>    The interval on which to print CSV metric rows (default 100)
+    -a,--alloc <min> <max>        The, inclusive, minimum and maximum size class which can be randomly allocated (default {min_size_class} {max_size_class})
+    -c,--csv-header               Print CSV header row first
+    -C,--only-csv-header          Print CSV header row and exit
+
+BEHAVIOR
+
+    Randomly allocates bytes and outputs metrics as CSV table rows.
+
+", min_size_class=MIN_SIZE_CLASS, max_size_class=MAX_SIZE_CLASS);
+    }
+}
+
+/// Allocate and free a lot of times.
+#[cfg(feature = "metrics")]
+fn main() {
+    // Parse command line arguments
+    let mut args: Vec<String> = env::args().collect();
+    args.reverse();
+    args.pop().unwrap(); // Remove binary name
+    
+    let parsed_args = Args::new(&mut args);
+
+    if let Some(print) = parsed_args.print_usage {
+        if print {
+            Args::print_usage();
+            exit(0);
+        }
+    }
+
+    if let Some(status) = parsed_args.print_csv_header {
+        println!("iteration,total_alloc_bytes,total_minipages,heap_bytes_write,heap_bytes_read,total_allocs,total_deallocs,fresh_allocs,reused_allocs");
+        
+        match status {
+            PrintCSVHeader::Exit => exit(0),
+            _ => {},
+        }
+    }
+
+    // Run benchmark
+    let mut benchmark = RandomReport{
+        rng: thread_rng(),
+        free_later: vec!(),
+        iteration: 0,
+        total_alloc_bytes: 0,
+        alloc_range: parsed_args.alloc_range.unwrap(),
     };
 
-    print_metrics(arg_iterations+1, metrics, ratio, total_alloc_bytes);
+    for _i in 0..=parsed_args.max_iterations.unwrap() {
+        unsafe {
+            benchmark.iterate();
+        }
+
+        // Print metrics
+        if benchmark.iteration % parsed_args.report_interval.unwrap() == 0 {
+            unsafe {
+                benchmark.print_metrics();
+            }
+        }
+    }
+
+    unsafe {
+        benchmark.cleanup();
+    }
 }
